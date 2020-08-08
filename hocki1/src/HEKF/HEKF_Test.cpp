f4244d7adaf499c03318d2ee077da98c5c2e9b5f
smtm1209 <sarkistm@hotmail.com>
2020-08-08 15:51:53 -0400

    Merge kill/unkill sensor abilities

--- hocki1/src/HEKF/HEKF_Test.cpp
+++ hocki1/src/HEKF/HEKF_Test.cpp
@@ -7,13 +7,17 @@ using namespace Eigen;
 
 int main() {
 	HEKF* filter = initHEKF(1e-6, 2, 4, 1.5, 5.5);
-	Matrix<float, 6, 1> meas;
-	meas << 2, 2, 2, 2, 2, 2;
-	for(unsigned int i = 1; i <= 10; i++) {
-		if(updateHEKF(filter, meas, 6, 100000 * i, ACC) == 1) {
-			std::cout << filter->x << std::endl;
-		} else {
-			std::cout << "Error: Sensor and measurement mismatch" << std::endl;
-		}
-	}
+	killSensor(filter, ACC, 1);
+	reviveSensor(filter, ACC, 1);
+	std::cout << filter->RTable[0] << std::endl;
+
+	// Matrix<float, 6, 1> meas;
+	// meas << 2, 2, 2, 2, 2, 2;
+	// for(unsigned int i = 1; i <= 10; i++) {
+	// 	if(updateHEKF(filter, meas, 6, 100000 * i, ACC) == 1) {
+	// 		std::cout << filter->x << std::endl;
+	// 	} else {
+	// 		std::cout << "Error: Sensor and measurement mismatch" << std::endl;
+	// 	}
+	// }
 }
