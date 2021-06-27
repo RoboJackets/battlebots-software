7d17097d5a4533e028f2266c935b7590c7816db9
Varun <vmadabushi3@gatech.edu>
2021-06-27 11:45:03 -0400

    Drivetrain class

---
+++ Hocki_PIO/lib/DriveTrain/DriveTrain.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include "DriveTrain.h"
+
+
+DriveTrain::DriveTrain(int pinL, int pinR) : pinL{pinL}, pinR{pinR} {
+}
+
+void DriveTrain::init() {
+    pinMode(pinL, OUTPUT);
+    pinMode(pinR, OUTPUT);
+    analogWriteResolution(12);
+    analogWriteFrequency(pinL, F_PWM);
+    analogWriteFrequency(pinR, F_PWM);
+
+    writeESC(pinL, 500);
+    writeESC(pinR, 500);
+}
+
+void DriveTrain::setPower(int powerLeft, int powerRight){
+    writeESC(pinL, powerLeft);
+    writeESC(pinR, powerRight);
+}
+
+/*
+Arms ESC according to https://github.com/bitdump/BLHeli/blob/master/BLHeli_32%20ARM/BLHeli_32%20manual%20ARM%20Rev32.x.pdf
+*/
+void DriveTrain::arm(){
+    for(int i = ESC_STOP_VAL; i < ESC_STOP_VAL + ESC_ARM_PEAK_VAL; i++) {
+        writeESC(pinL, i);
+        writeESC(pinR, i);
+        delay(10);
+    }
+    delay(100);
+    for(int i = ESC_STOP_VAL + ESC_ARM_PEAK_VAL; i >= ESC_STOP_VAL; i--) {
+        writeESC(pinL, i);
+        writeESC(pinR, i);
+        delay(10);
+    }
+}
+
+/* 
+Writes command to ESC using PWM. 0 is full backwards, 1000 is full forwards, 500 is stopped.
+This is consistent with the DSHOT command range (in case we decide to switch to DSHOT I guess)
+*/
+void DriveTrain::writeESC(int pin, int cmd){
+  int val = (1000+cmd) * PWM_RESOLUTION / (T_PWM*1000);
+  analogWrite(pin, val);
+}
--- Hocki_PIO/src/main.cpp
+++ Hocki_PIO/src/main.cpp
@@ -3,15 +3,30 @@
 #include "Controller.h"
 #include "ControllerPacket.h"
 #include "ADXL375.h"
+#include "DriveTrain.h"
 
 Controller c(&Serial1);
 ControllerPacket p;
+DriveTrain drive(ESC_L, ESC_R);
+
 void setup() {
   /* Serial to display the received data */
-  Serial.begin(115200);
-  while (!Serial) {}
+  SerialUSB.begin(115200);
+  while (!SerialUSB) {}
+  drive.init();
+  drive.arm();
 }
 
 void loop() {
   c.read(&p);
+
+  if(p.tankDrive){
+    SerialUSB.print("X Speed: ");
+    SerialUSB.print(p.xSpeed);
+    SerialUSB.print("  Y Speed: ");
+    SerialUSB.print(p.ySpeed);
+    SerialUSB.print("  Rot Speed: ");
+    SerialUSB.println(p.rotSpeed);
+    drive.setPower(p.xSpeed + p.rotSpeed, p.xSpeed - p.rotSpeed);
+  }
 }
