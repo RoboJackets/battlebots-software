30604b0b1afea82ad547374ab1ffb5f13bb06b3e
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2021-07-18 20:40:11 -0700

    Adding Eigentest

---
+++ Hocki_PIO/test/Eigentest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../include/eigen/include/Eigen/Dense"
+
+
+using namespace std;
+ 
+int main()
+{
+  Eigen::MatrixXf m(2, 2); //initializes a 2x2 dynamic matrix m that takes floats
+  m(0,0) = 2.4; //sets values for matrix m
+  m(0,1) = 3.1;
+  m(1,0) = 1.0;
+  m(1,1) = 0.2;
+
+  Eigen::MatrixXf n(2, 2); //initializes 2x2 dynamic matrix n that takes floats
+  n(0,0) = 1.3; //sets values for matrix n
+  n(0,1) = 4.5;
+  n(1,0) = 2.7;
+  n(1,1) = 6.2;
+
+  std::cout << "Here is matrix m * n\n" <<m*n<<std::endl;
+  std::cout << "Here is the inverse of m\n" <<m.inverse()<<std::endl;
+
+}
