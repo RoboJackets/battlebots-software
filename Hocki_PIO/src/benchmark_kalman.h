6198779e2f684033e1eb11ab1a9329c4d3fcd10e
Varun <vmadabushi3@gatech.edu>
2021-11-18 23:03:44 -0500

    Added build flag to platformio.ini, fixed compilation issue in KF update

--- Hocki_PIO/src/benchmark_kalman.h
+++ Hocki_PIO/src/benchmark_kalman.h
@@ -9,7 +9,7 @@
 #include "KalmanFilter.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <chrono>
+//#include <chrono>
 
 void setup();
 void loop();
@@ -26,15 +26,12 @@ void loop() {
     while (count < 10000) {
         Eigen::Vector4f measured = Eigen::Vector4f::Random();
 
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
         k.update(measured, 0.1);
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-        std::chrono::duration<float> fs = end - begin;
 
         SerialUSB.print("OP Took: ");
-        SerialUSB.print(fs.count());
-        sum = sum + fs.count();
+        //SerialUSB.print(fs.count());
+        //sum = sum + fs.count();
         count++;
     }
     SerialUSB.print("Final Average: ");
